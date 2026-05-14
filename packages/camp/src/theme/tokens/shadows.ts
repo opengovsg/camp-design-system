57c0e1fc5407c4a81d50353d623c53041ac8579d
Kar Rui <karrui@open.gov.sg>
2026-05-14 15:00:29 +0800

    feat(camp): port shadow tokens to v3

---
+++ packages/camp/src/theme/tokens/shadows.ts
@@ -0,0 +1,6 @@
+import { defineTokens } from '@chakra-ui/react'
+
+export const shadows = defineTokens.shadows({
+  sm: { value: '0px 0px 10px 0px rgba(191, 191, 191, 0.5)' },
+  md: { value: '0px 0px 20px 0px rgba(104, 104, 104, 0.3)' },
+})
