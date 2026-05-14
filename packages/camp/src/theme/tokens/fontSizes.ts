63cedf445854c8d26f2dbef408421dd8d0c483f8
Kar Rui <karrui@open.gov.sg>
2026-05-14 15:01:03 +0800

    feat(camp): port typography tokens to v3

---
+++ packages/camp/src/theme/tokens/fontSizes.ts
@@ -0,0 +1,20 @@
+import { defineTokens } from '@chakra-ui/react'
+
+export const fontSizes = defineTokens.fontSizes({
+  '2xs': { value: '0.625rem' },
+  xs: { value: '0.75rem' },
+  sm: { value: '0.875rem' },
+  md: { value: '1rem' },
+  lg: { value: '1.125rem' },
+  xl: { value: '1.25rem' },
+  '2xl': { value: '1.5rem' },
+  '3xl': { value: '1.75rem' },
+  '4xl': { value: '2rem' },
+  '5xl': { value: '2.25rem' },
+  '6xl': { value: '2.5rem' },
+  '7xl': { value: '2.75rem' },
+  '8xl': { value: '3rem' },
+  '9xl': { value: '3.5rem' },
+  '10xl': { value: '4rem' },
+  '11xl': { value: '4.5rem' },
+})
---
+++ packages/camp/src/theme/tokens/fontWeights.ts
@@ -0,0 +1,9 @@
+import { defineTokens } from '@chakra-ui/react'
+
+export const fontWeights = defineTokens.fontWeights({
+  light: { value: 300 },
+  normal: { value: 400 },
+  medium: { value: 500 },
+  semibold: { value: 600 },
+  bold: { value: 700 },
+})
