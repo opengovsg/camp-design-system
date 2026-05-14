59680764cb9445ce4c26084c9328ec3c096ff804
Kar Rui <karrui@open.gov.sg>
2026-05-14 15:00:14 +0800

    feat(camp): port spacing and sizes tokens to v3

---
+++ packages/camp/src/theme/tokens/spacing.ts
@@ -0,0 +1,18 @@
+import { defineTokens } from '@chakra-ui/react'
+
+export const spacing = defineTokens.spacing({
+  '1': { value: '0.25rem' },
+  '2': { value: '0.5rem' },
+  '3': { value: '0.75rem' },
+  '4': { value: '1rem' },
+  '5': { value: '1.25rem' },
+  '6': { value: '1.5rem' },
+  '7': { value: '1.75rem' },
+  '8': { value: '2rem' },
+  '9': { value: '2.25rem' },
+  '10': { value: '2.5rem' },
+  '11': { value: '2.75rem' },
+  '12': { value: '3rem' },
+  '13': { value: '3.5rem' },
+  '14': { value: '4rem' },
+})
